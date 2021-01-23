94c59d8d03fb9d2b3d90de4ec0b4a814edbad241
Stephen McBride <stevemcbride3@gmail.com>
2021-01-23 00:26:30 -0500

    Added dark mode feature

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Logo from '../logo.svg';
 import LogoBackground from '../logo-background.svg';
 import LogoForeground from '../logo-foreground.svg';
@@ -9,14 +9,49 @@ import EducationDetailsContainer from './EducationDetailsContainer';
 import './Home.css';
 
 export default function Home() {
+    const toggleTheme = () => {
+        let aArray = document.getElementsByTagName("a");
+        for (let i = 0; i < aArray.length; i++) {
+            aArray[i].classList.toggle("theme-dark-a");
+        }
+        
+        let pArray = document.getElementsByTagName("p");
+        for (let i = 0; i < pArray.length; i++) {
+            pArray[i].classList.toggle("theme-dark-p");
+        }
+        
+        let h1Array = document.getElementsByTagName("h1");
+        for (let i = 0; i < h1Array.length; i++) {
+            h1Array[i].classList.toggle("theme-dark-h1");
+        }
+
+        let h1DecorArray = document.getElementsByClassName("decor-text");
+        for (let i = 0; i < h1DecorArray.length; i++) {
+            h1DecorArray[i].classList.toggle("theme-dark-h1-decor-text");
+        }
+
+        let h2Array = document.getElementsByTagName("h2");
+        for (let i = 0; i < h2Array.length; i++) {
+            h2Array[i].classList.toggle("theme-dark-h2");
+        }
+
+        let lineArray = document.getElementsByClassName("line");
+        for (let i = 0; i < lineArray.length; i++) {
+            lineArray[i].classList.toggle("theme-dark-line");
+        }
+
+        document.body.classList.toggle("theme-dark-body");
+    }
     return (
         <>
             <header>
-                <div className="logos">
-                    <img src={Logo} className="header-logo"/>
-                    <img src={LogoBackground} className="header-logo-background"/>
-                    <img src={LogoForeground} className="header-logo-foreground"/>
-                </div>
+                <a id="logo-container" onClick={toggleTheme} href="#">
+                    <div className="logos">
+                        <img src={Logo} className="header-logo"/>
+                        <img src={LogoBackground} className="header-logo-background"/>
+                        <img src={LogoForeground} className="header-logo-foreground"/>
+                    </div>
+                </a>
                 <nav className="nav_links">
                     <ul>
                         <li>
